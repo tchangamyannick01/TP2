aacb7fae28df39036c7cf7a91b3095867405c888
tchangamyannick01 <tchangamyannick01@gmail.com>
2025-12-11 20:35:17 -0500

    Fonction d’initialisation

--- exercice2.js
+++ exercice2.js
@@ -2,3 +2,13 @@ const URL_CDP_K = 'http://localhost:8080/data_cdp_k.php'
 const URL_DEFINITIONS = 'http://localhost:8080/definition_forces.php'
 
 let cdpKData = null
+window.addEventListener('DOMContentLoaded', async () => {
+  try {
+    await chargerCdpK()
+    await chargerDefinitions()
+    configurerFormulaire()
+  } catch (err) {
+    console.error(err)
+    alert('Erreur de chargement des données. Vérifie Docker / URLs.')
+  }
+})
