6413679bd9ab204733b2c930af4fd8ed6c983fcc
tchangamyannick01 <tchangamyannick01@gmail.com>
2025-12-11 21:08:32 -0500

    Génération de la grille HTML

--- exercice1.js
+++ exercice1.js
@@ -21,3 +21,39 @@ async function chargerParametres () {
 
   return json 
 }
+function genererGrille (params) {
+  const nbLignes = params.nombre_lignes
+  const nbColonnes = params.nombre_colonnes
+
+  const conteneur = document.querySelector('#conteneur-grille')
+  const resultat = document.querySelector('#texte-resultat')
+
+  const table = document.createElement('table')
+  table.className = 'table table-bordered text-center align-middle'
+
+  let compteurSup80 = 0
+
+  for (let i = 0; i < nbLignes; i++) {
+    const tr = document.createElement('tr')
+
+    for (let j = 0; j < nbColonnes; j++) {
+      const td = document.createElement('td')
+      const nombre = _.random(10, 90)
+      td.textContent = nombre
+      if (nombre > 80) {
+        compteurSup80++
+        td.style.backgroundColor = 'rgb(5,173,131)'
+        td.style.color = 'rgb(255,255,255)'
+        td.style.fontWeight = 'bold'
+      }
+
+      tr.appendChild(td)
+    }
+
+    table.appendChild(tr)
+  }
+  conteneur.innerHTML = ''
+  conteneur.appendChild(table)
+  resultat.textContent =
+    `Nombre de cellules ayant une valeur supérieure à 80 : ${compteurSup80}`
+}
